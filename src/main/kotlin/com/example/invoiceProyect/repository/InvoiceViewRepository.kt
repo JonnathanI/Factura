a86082360078975e0ee0bc38f8cbffc362299d71
JonnathanI <jjinaguazo@sudamericano.edu.ec>
2024-06-14 10:51:19 -0500

    Completo

--- src/main/kotlin/com/example/invoiceProyect/repository/InvoiceViewRepository.kt
+++ src/main/kotlin/com/example/invoiceProyect/repository/InvoiceViewRepository.kt
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface InvoiceViewRepository: JpaRepository<InvoiceView, Long> {
+interface   InvoiceViewRepository: JpaRepository<InvoiceView, Long> {
     fun findById (id: Long?): InvoiceView?
 }
