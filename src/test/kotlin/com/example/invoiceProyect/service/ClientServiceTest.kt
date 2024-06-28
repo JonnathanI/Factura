7d6df0d7e2389a7a2920e421e7956f9fb994b74f
JonnathanI <jjinaguazo@sudamericano.edu.ec>
2024-06-28 11:15:14 -0500

    Completo

--- src/test/kotlin/com/example/invoiceProyect/service/ClientServiceTest.kt
+++ src/test/kotlin/com/example/invoiceProyect/service/ClientServiceTest.kt
@@ -1,7 +1,10 @@
 package com.example.invoiceProyect.service
 
 import com.example.invoiceProyect.model.Client
+import com.example.invoiceProyect.model.Invoice
 import com.example.invoiceProyect.repository.ClientRepository
+import com.google.gson.Gson
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
@@ -9,6 +12,7 @@ import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.springframework.boot.test.context.SpringBootTest
+import java.io.File
 
 @SpringBootTest
 class ClientServiceTest {
@@ -19,20 +23,17 @@ class ClientServiceTest {
     @Mock   // objeto simulado
     lateinit var clientRepository: ClientRepository
 
-    val clientMock = Client().apply {
-        id = 1
-        nui = "0301707030"
-        fullName = "Juan"
-        address = "Cuenca"
-    }
+    val jsonString = File("./src/test/resources/client/invoice.json").readText(Charsets.UTF_8)
+
+    val clientMock = Gson().fromJson(jsonString, Client::class.java)
 
     @Test
     fun saveClientCorrect() {
         Mockito.`when`(clientRepository.save(Mockito.any(Client::class.java))).thenReturn(clientMock)
         val response = clientService.save(clientMock)
-        assertEquals(response.id, clientMock.id)
+        Assertions.assertEquals(response.id, clientMock.id)
     }
-
+/*
     @Test
     fun saveClientWhenFullNameIsBlank() {
         assertThrows(Exception::class.java) {
@@ -40,5 +41,36 @@ class ClientServiceTest {
             Mockito.`when`(clientRepository.save(Mockito.any(Client::class.java))).thenReturn(clientMock)
             clientService.save(clientMock)
         }
+    }
+
+ */
+    @Test
+    fun saveWhenNuiClientIsCorrect(){
+        Mockito.`when`(clientRepository.save(Mockito.any(Client::class.java))).thenReturn(clientMock)
+        val response = clientService.validarNui(clientMock.nui)
+        Assertions.assertEquals(true, response)
+    }
+    @Test
+    fun saveWhenNuiClientIsIncorrect(){
+        clientMock.nui="xbhytragio"
+        Mockito.`when`(clientRepository.save(Mockito.any(Client::class.java))).thenReturn(clientMock)
+        val response = clientService.validarNui(clientMock.nui)
+        Assertions.assertEquals(false, response)
+    }
+
+    @Test
+    fun saveWhenNuiClientIsBlanco(){
+        clientMock.nui=""
+        Mockito.`when`(clientRepository.save(Mockito.any(Client::class.java))).thenReturn(clientMock)
+        val response = clientService.validarNui(clientMock.nui)
+        Assertions.assertEquals(false, response)
+    }
+
+    @Test
+    fun saveWhenNuiClientIsIncoplete(){
+        clientMock.nui="05462897"
+        Mockito.`when`(clientRepository.save(Mockito.any(Client::class.java))).thenReturn(clientMock)
+        val response = clientService.validarNui(clientMock.nui)
+        Assertions.assertEquals(false, response)
     }
 }
